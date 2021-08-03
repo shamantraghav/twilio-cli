6bb506ed23d5c291855d4a294902d60c078c0ea8
Shamant Raghav <sraghav@twilio.com>
2021-08-03 16:49:13 +0530

    Update changes
    SR: Update changes

--- test/hooks/plugin-install.test.js
+++ test/hooks/plugin-install.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const sinon = require('sinon');
 const inquirer = require('inquirer');
 const { expect, test } = require('@twilio/cli-test');
@@ -31,6 +32,7 @@ const getUndefinedPlugin = () => ({
 describe('hooks', () => {
   describe('plugin-install', () => {
     before(() => {
+      warnSpy = sinon.spy(console, 'warn');
       inquirer._prompt = inquirer.prompt;
       inquirer.prompt = sinon.stub().resolves({ continue: false });
     });
@@ -38,6 +40,7 @@ describe('hooks', () => {
     after(() => {
       inquirer.prompt = inquirer._prompt;
       delete inquirer._prompt;
+      warnSpy.restore();
     });
 
     test.stderr().it('warning when non Twilio plugin is installed', async (ctx) => {
@@ -54,9 +57,9 @@ describe('hooks', () => {
 
     test.stderr().it('outputs nothing when Twilio plugin is installed', async (ctx) => {
       await pluginFunc.call(ctx, getTwilioPlugin());
-      expect(ctx.stderr).to.be.empty;
+      expect(console.warn.calledWith(sinon.match('twilio autocomplete'))).to.be.true;
       await pluginFunc.call(ctx, getTwilioLabsPlugin());
-      expect(ctx.stderr).to.be.empty;
+      expect(console.warn.calledWith(sinon.match('twilio autocomplete'))).to.be.true;
     });
   });
 });
